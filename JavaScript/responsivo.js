c5581596bfbc64fd80d61061aaa78089262f67b0
Maria Rita <mariaritacfwork@gmail.com>
2025-12-09 17:33:49 -0300

    Ajustando JS do menu mobile

--- JavaScript/responsivo.js
+++ JavaScript/responsivo.js
@@ -4,3 +4,17 @@ const menu = document.getElementById('menu');
 toggle.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+
+
+document.querySelectorAll('.nav a').forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
+});
+
+
+document.addEventListener('click', (e) => {
+  if (!menu.contains(e.target) && !toggle.contains(e.target)) {
+    menu.classList.remove('active');
+  }
+});
